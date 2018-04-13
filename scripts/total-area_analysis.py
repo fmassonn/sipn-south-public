b1c50f5ebdfecf0387849d5f37de9265ba6869d1
fmasson <francois.massonnet@uclouvain.be>
2018-04-13 15:16:59 +0200

    total area

--- scripts/total-area_analysis.py
+++ scripts/total-area_analysis.py
@@ -35,30 +35,35 @@ col = [info[j_sub][2] for j_sub in range(n_sub)]
 plt.figure(figsize = (6, 4))
 
 for j_sub in range(n_sub):
-  plt.text(29, 4.0 - j_sub * 0.3, info[j_sub][0] + " " + info[j_sub][3], color = col[j_sub])
+  #plt.text(29, 5.0 - j_sub * 0.3, info[j_sub][0] + " " + info[j_sub][3], color = col[j_sub])
   for j_for in range(n_for[j_sub]):
     # Total area
     filein = "../data/txt/" + sub_id[j_sub] + "_" + str(j_for + 1).zfill(3) + "_total-area.txt"
     # Read the CSV file
     csv = pd.read_csv(filein, header = None)
     series = csv.iloc[0][:]
-    # Plot series, line thinner for large ensembles
-    plt.plot(np.arange(len(time)) + 1, series, color = col[j_sub], lw = (5.0 / n_for[j_sub]) ** 0.2 )
+    # Plot series, line thinner for large ensembles. Legend only if first member
+     
+    if j_for == 0:
+      mylab = info[j_sub][0] + " " + info[j_sub][3]
+    else:
+      mylab = "_nolegend_"
+
+    plt.plot(np.arange(len(time)) + 1, series, color = col[j_sub], lw = (5.0 / n_for[j_sub]) ** 0.2, label = mylab)
 
 # Plot observations
 # -----------------
 filein = "../data/txt/NSIDC-0081_000_total-area.txt"
 csv = pd.read_csv(filein, header = None)
 series = csv.iloc[0][:]
-plt.plot(np.arange(len(time)) + 1, series, color = [0.1, 0.1, 0.1], lw = 3.0, linestyle = "--")
-plt.text(29, 4.0 - n_sub * 0.3, "OBS (NSIDC 0081)", color = "black")
+plt.plot(np.arange(len(time)) + 1, series, color = [0.1, 0.1, 0.1], lw = 3.0, linestyle = "--", label = "OBS (NSIDC-0081)")
 
 filein = "../data/txt/OSI-401-b_000_total-area.txt"
 csv = pd.read_csv(filein, header = None)
 series = csv.iloc[0][:]
-plt.plot(np.arange(len(time)) + 1, series, color = [0.1, 0.1, 0.1], lw = 3.0, linestyle = ":")
-plt.text(29, 4.0 - n_sub * 0.3, "OBS (OSI-401-b)", color = "black")
+plt.plot(np.arange(len(time)) + 1, series, color = [0.1, 0.1, 0.1], lw = 3.0, linestyle = ":", label = "OBS (OSI-401-b)")
 
+plt.legend(fontsize = 8)
 
 plt.title("Forecast of February 2018 total\nAntarctic sea ice area")
 plt.xticks([1, 5, 10, 15, 20, 25, 28], ["01", "05", "10", "15", "20", "25", "28"])
