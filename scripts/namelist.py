9bbe77dc73635bf1ef8ff9ffc4a4d8988d2f14df
François Massonnet <francois.massonnet@uclouvain.be>
2018-04-13 14:46:04 +0200

    add namelist

---
+++ scripts/namelist.py
@@ -0,0 +1,20 @@
+#          Group     Nb forecasts  Color for plots     Comment
+#          name
+np.random.seed(10)
+info = [ ["nrl"     ,6            ,"#92B558",   ""],  \
+         ["Nico-Sun",1            ,"#DC4C46",  ""],  \
+         ["FIO-ESM", 1            ,"#672E3B",  ""],  \
+         ["ecmwf",   50           ,"#F3D6E4",  ""],  \
+         ["Gateway", 1            ,"#C48F65",  ""],  \
+         ["mpas-cesm",2           ,"#223A5E",  ""],  \
+         ["Modified-CanSIPS",20   ,"#898E8C",  ""],  \
+         ["NASA-GSFC",1           ,"#005960",  "(monthly)"],\
+         ["Lamont",  1            ,"#9C9A40",  "(monthly)"],\
+         ["nasa-gmao",10          ,"#4F84C4", ""],\
+         ["MetOffice",42          ,"#D2691E", ""], \
+         ["ucl",      10          ,"#F6D155", ""], \
+         ["emc",      15          ,"#004B8D", ""], \
+       ]
+
+
+
