29eec66abc801c5224aabe07874789da42312de9
François Massonnet <francois.massonnet@uclouvain.be>
2018-04-13 14:38:16 +0200

    rename retrieval script

---
+++ scripts/retrieve_data.bash
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+Browse  https://nextcloud.cism.ucl.ac.be/s/oMqpuF3nWvL1O2c
+
+Password: ask Francois Massonnet
+
+Download the file ./2018/netcdf (it will come as a zip), unzip it in ../data/
+
+This netcdf folder contains forecast and obs data
--- scripts/retrieve_forecasts.bash
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/bash
-wget -N -c https://nextcloud.cism.ucl.ac.be/s/oMqpuF3nWvL1O2c -p ../data/netcdfs
