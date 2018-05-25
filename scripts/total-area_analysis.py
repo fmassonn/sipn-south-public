8e61fcbbcd4f24ef2cad3ff6047e773c829d8ca3
fmasson <francois.massonnet@uclouvain.be>
2018-05-25 15:09:54 +0200

    Polish total area analyses, add estimations of the minimum

--- scripts/total-area_analysis.py
+++ scripts/total-area_analysis.py
@@ -14,6 +14,7 @@ import os
 from netCDF4 import Dataset
 from datetime import date
 
+plt.close("all")
 # Load namelist
 exec(open("./namelist.py").read())
 
@@ -32,10 +33,10 @@ n_for = [info[j_sub][1] for j_sub in range(n_sub)]
 # Colors
 col = [info[j_sub][2] for j_sub in range(n_sub)]
 
-plt.figure(figsize = (6, 4))
+plt.figure("fig1", figsize = (6, 4))
+plt.figure("fig2", figsize = (6, 4))
 
 for j_sub in range(n_sub):
-  #plt.text(29, 5.0 - j_sub * 0.3, info[j_sub][0] + " " + info[j_sub][3], color = col[j_sub])
   for j_for in range(n_for[j_sub]):
     # Total area
     filein = "../data/txt/" + sub_id[j_sub] + "_" + str(j_for + 1).zfill(3) + "_total-area.txt"
@@ -49,22 +50,48 @@ for j_sub in range(n_sub):
     else:
       mylab = "_nolegend_"
 
+    plt.figure("fig1")
     plt.plot(np.arange(len(time)) + 1, series, color = col[j_sub], lw = (5.0 / n_for[j_sub]) ** 0.2, label = mylab)
 
+
+    # Record when the minimum of the series occurs
+    # --------------------------------------------
+    # Fit a quadratic polynomial to filter out weather variability
+    tt = np.arange(0.0, len(series), 0.1)
+    daymin = 1 + tt[np.argmin(np.polyval(np.polyfit(range(len(series)), series, 2), tt))]
+
+    plt.figure("fig2")
+    if info[j_sub][3] != "(monthly)":
+      plt.scatter(daymin, n_sub - j_sub, color = col[j_sub], label = mylab)
+      #plt.text(46, n_sub - j_sub, info[j_sub][0] + " " + info[j_sub][3], color = col[j_sub], ha = "right")
+
+ 
 # Plot observations
 # -----------------
 filein = "../data/txt/NSIDC-0081_000_total-area.txt"
 csv = pd.read_csv(filein, header = None)
 series = csv.iloc[0][:]
+plt.figure("fig1")
 plt.plot(np.arange(len(time)) + 1, series, color = [0.1, 0.1, 0.1], lw = 3.0, linestyle = "--", label = "OBS (NSIDC-0081)")
+plt.figure("fig2")
+tt = np.arange(0.0, len(series), 0.1)
+daymin = 1 + tt[np.argmin(np.polyval(np.polyfit(range(len(series)), series, 2), tt))]
+plt.plot((daymin, daymin), (0, n_sub), color = [0.1, 0.1, 0.1], linestyle = "--", label = "OBS (NSIDC-0081)")
+
 
 filein = "../data/txt/OSI-401-b_000_total-area.txt"
 csv = pd.read_csv(filein, header = None)
 series = csv.iloc[0][:]
+plt.figure("fig1")
 plt.plot(np.arange(len(time)) + 1, series, color = [0.1, 0.1, 0.1], lw = 3.0, linestyle = ":", label = "OBS (OSI-401-b)")
-
+plt.legend(fontsize = 8)
+plt.figure("fig2")
+tt = np.arange(0.0, len(series), 0.1)
+daymin = 1 + tt[np.argmin(np.polyval(np.polyfit(range(len(series)), series, 2), tt))]
+plt.plot((daymin, daymin), (0, n_sub), color = [0.1, 0.1, 0.1], linestyle = ":", label = "OBS (OSI-401-b)")
 plt.legend(fontsize = 8)
 
+plt.figure("fig1")
 plt.title("February 2018 total Antarctic sea ice area")
 plt.xticks([1, 5, 10, 15, 20, 25, 28], ["01", "05", "10", "15", "20", "25", "28"])
 plt.xlabel("Day in February 2018")
@@ -75,3 +102,15 @@ plt.grid()
 plt.tight_layout()
 plt.savefig("../figs/fig1.png", dpi = 500)
 print("Figure ../figs/fig1.png printed")
+
+
+plt.figure("fig2")
+plt.title("When did the 2018 minimum of Antarctic sea ice area occur?")
+plt.xlim(0.0, 46)
+plt.xticks([1, 5, 10, 15, 20, 25, 28], ["<01", "05", "10", "15", "20", "25", ">28"])
+plt.xlabel("Day in February 2018")
+plt.yticks([],[])
+plt.tight_layout()
+plt.savefig("../figs/fig2.png", dpi = 500)
+print("Figure ../figs/fig2.png printed")
+
