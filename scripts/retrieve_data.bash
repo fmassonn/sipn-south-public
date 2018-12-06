adeec8c9b968e60f1bd647636d9349d09077f6bf
François Massonnet <francois.massonnet@uclouvain.be>
2018-12-06 15:05:22 +0100

    Change scripts to account for new hindcasts

--- scripts/retrieve_data.bash
+++ scripts/retrieve_data.bash
@@ -5,11 +5,16 @@ set -o errexit
 # Downloads the SIPN South 2018 forecast data at the appropriate location
 #
 # F. Massonnet, June 2018
+#               December 2019: update to download 2018-2019 data
+
 
 url=https://nextcloud.cism.ucl.ac.be/s/gTL53xhjp4iQMM8
 
-for file in `cat list_files.txt`
-do
-  wget -N -c ${url}/download?path=%2F2018/netcdf/$file -O ../data/netcdf/$file
+for year in `seq 2017 2018`
+  mystring="$year-$((year + 1))"
+  for file in `cat list_files_${mystring}.txt`
+  do
+    wget -N -c ${url}/download?path=%2F${mystring}/netcdf/$file -O ../data/netcdf/${my_string}/$file
+  done
 done
 
