6ac5288397bb45a0e02003296e0c31bf1dbf5320
François Massonnet <francois.massonnet@uclouvain.be>
2018-12-10 10:42:40 +0100

    Update retrieve_data.bash to work in practice

--- scripts/retrieve_data.bash
+++ scripts/retrieve_data.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -o nounset
 set -o errexit
-
+set -x
 # Downloads the SIPN South 2018 forecast data at the appropriate location
 #
 # F. Massonnet, June 2018
@@ -10,11 +10,12 @@ set -o errexit
 
 url=https://nextcloud.cism.ucl.ac.be/s/gTL53xhjp4iQMM8
 
-for mystring in "2017-2018 2018-2019"
+
+for mystring in 2018-2019
   do
   for file in `cat list_files_${mystring}.txt`
   do
-    wget -N -c ${url}/download?path=%2F${mystring}/netcdf/$file -O ../data/netcdf/${my_string}/$file
+    wget -N -c  ${url}/download?path=%2F${mystring}/netcdf/$file -O ../data/${mystring}/netcdf/$file
   done
 done
 
