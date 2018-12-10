5aec93733a0ea21df8170fc97fbe2d53389653cb
François Massonnet <francois.massonnet@uclouvain.be>
2018-12-10 09:56:29 +0100

    Add Petty, ECMWF, update namelist

--- scripts/namelist_2018-2019.py
+++ scripts/namelist_2018-2019.py
@@ -4,12 +4,12 @@ np.random.seed(10)
 info = [ ["nrl"     ,1            ,"#92B558",   ""],  \
          ["Nico-Sun",1            ,"#DC4C46",  ""],  \
          ["FIO-ESM", 1            ,"#672E3B",  ""],  \
-         #["ecmwf",   50           ,"#F3D6E4",  ""],  \
+         ["ecmwf",   50           ,"#F3D6E4",  ""],  \
          #["Gateway", 1            ,"#C48F65",  ""],  \
          ["CMCC"    , 50          ,"#2E8B57",   ""],  \
          #["mpas-cesm",2           ,"#223A5E",  ""],  \
          #["Modified-CanSIPS",20   ,"#898E8C",  ""],  \
-         #["NASA-GSFC",1           ,"#005960",  "(monthly)"],\
+         ["Petty-NASA",1           ,"#005960",  "(interpolated)"],\
          ["Lamont",  1            ,"#9C9A40",  "(interpolated)"],\
          ["nasa-gmao",10          ,"#4F84C4", ""],\
          ["MetOffice",42          ,"#D2691E", ""], \
