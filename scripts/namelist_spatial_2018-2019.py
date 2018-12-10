da3b3cadfe549baa284c174fadbc9f7fe3863dfb
François Massonnet <francois.massonnet@uclouvain.be>
2018-12-10 17:08:13 +0100

    add nrl namelist spatial

--- scripts/namelist_spatial_2018-2019.py
+++ scripts/namelist_spatial_2018-2019.py
@@ -1,10 +1,11 @@
 #          Group     Nb forecasts  Color for plots     Comment
 #          name
 np.random.seed(10)
-info = [ #["nrl"     ,6            ,"#92B558",   ""],  \
+info = [ ["nrl"     ,6            ,"#92B558",   ""],  \
          ["Lamont",  1            ,"#9C9A40",  "(interpolated)"],\
-         #["nasa-gmao",10          ,"#4F84C4", ""],\
-         #["MetOffice",42          ,"#D2691E", ""], \
+         ["nasa-gmao",10          ,"#4F84C4", ""],\
+         ["MetOffice",42          ,"#D2691E", ""], \
+         ["Nico-Sun", 3          , "#DC4C46", ""], \
          #["ucl",      10          ,"#F6D155", ""], \
          #["emc",      15          ,"#004B8D", ""], \
        ]
