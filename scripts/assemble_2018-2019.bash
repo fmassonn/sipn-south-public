f890e2f1509051fc30f2493223c3ab81b89d16d7
François Massonnet <francois.massonnet@uclouvain.be>
2019-05-02 09:16:09 +0200

    V2 of submitted SIPN South report

--- scripts/assemble_2018-2019.bash
+++ scripts/assemble_2018-2019.bash
@@ -13,7 +13,6 @@ cd ../figs
 convert +append nrl_ens-mean_concentration_Feb2019-mean.png \
                 nasa-gmao_ens-mean_concentration_Feb2019-mean.png \
                 MetOffice_ens-mean_concentration_Feb2019-mean.png \
-                barreira_ens-mean_concentration_Feb2019-mean.png \
                 X.png
 
 convert +append Lamont_ens-mean_concentration_Feb2019-mean.png \
@@ -21,8 +20,11 @@ convert +append Lamont_ens-mean_concentration_Feb2019-mean.png \
                 ucl_ens-mean_concentration_Feb2019-mean.png \
                 Y.png
 
-convert -append X.png Y.png monthly_mean.png
-rm -f X.png Y.png
+convert +append  barreira_ens-mean_concentration_Feb2019-mean.png \
+                Z.png
+
+convert -append X.png Y.png Z.png  monthly_mean.png
+rm -f X.png Y.png Z.png
 
 # Movie
 
@@ -34,17 +36,18 @@ do
   convert +append nrl_prob-15_concentration_d${dd}.png \
                   nasa-gmao_prob-15_concentration_d${dd}.png \
                   MetOffice_prob-15_concentration_d${dd}.png \
-		  barreira_prob-15_concentration_d${dd}.png \
                   ${dd}_X.png
 
   convert +append Lamont_prob-15_concentration_d${dd}.png \
                   Nico-Sun_prob-15_concentration_d${dd}.png \
                   ucl_prob-15_concentration_d${dd}.png     \
                   ${dd}_Y.png
+  convert +append barreira_prob-15_concentration_d${dd}.png \
+                  ${dd}_Z.png
+ 
+  convert -append ${dd}_X.png ${dd}_Y.png ${dd}_Z.png ${dd}.png
 
-  convert -append ${dd}_X.png ${dd}_Y.png ${dd}.png
-
-  rm -f ${dd}_X.png ${dd}_Y.png 
+  rm -f ${dd}_X.png ${dd}_Y.png ${dd}_Z.png
 done
 
 convert -delay 50 ??.png probability.gif
