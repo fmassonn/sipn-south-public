68f7ab443003f13bce11ea7a01c583cc864e0b03
François Massonnet <massonnetf@mac-1T0-417.local>
2022-01-10 15:17:11 +0100

    paper - time series and start sectors

---
+++ scripts/sectorsDefinitions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 10 15:05:55 2022
+
+@author: massonnetf
+"""
+
+#           Name                       Lon western  Lon eastern
+
+sectors = [
+           ["Weddell",                 -60.0,       20.0  ],
+           ["Indian",                  20.0 ,       90.0  ], 
+           ["West Pacific",            90.0 ,       160.0 ],
+           ["Ross",                    160.0,       -130.0],
+           ["Amundsen-Bellingshausen", -130.0,      -60.0 ],
+           ["Southern Ocean"         , -180.0,      180.0 ],
+          ]
+           
