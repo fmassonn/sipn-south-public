a182b31a0629a0ecbdd123d3d8c061f435875414
François Massonnet <francois.massonnet@uclouvain.be>
2022-01-11 12:19:46 +0100

    Renames, re-organisation of data

--- scripts/retrieve_NSIDC-0081.bash
+++ scripts/retrieve_NSIDC-0081.bash
@@ -14,7 +14,7 @@ for hemi in south
 do
   for month in 01 02 03 04 05 06 07 08 09 10 11 12
   do
-    for year in `seq 2020 2021`
+    for year in `seq 2015 2021`
     do
       echo ""
       wget -nc ftp://sidads.colorado.edu/pub/DATASETS/nsidc0081_nrt_nasateam_seaice/${hemi}/nt_${year}${month}??_f??_nrt_?.bin -P ${outdir}
