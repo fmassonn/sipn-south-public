89e15a166b3a5579e2f47b52bc94222f7f8797ec
François Massonnet <francois.massonnet@uclouvain.be>
2022-04-05 13:42:08 +0200

    Switch to post-season mode

--- scripts/total-area_analysis.py
+++ scripts/total-area_analysis.py
@@ -40,7 +40,7 @@ myyear = "2021-2022"
 plotobs = True
 
 # Are we after the period to be forecasted? (to know if need to plot verif)
-postseason = False
+postseason = True
 
 # Name of observational products      
 obs = ["NSIDC-0081", "OSI-401-b"]
