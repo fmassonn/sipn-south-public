cc943019af81a458bf0bed83d2756f71694fe642
François Massonnet <francois.massonnet@uclouvain.be>
2022-04-05 13:53:23 +0200

    update retrieve scripts

--- scripts/retrieve_data.bash
+++ scripts/retrieve_data.bash
@@ -2,16 +2,16 @@
 set -o nounset
 set -o errexit
 set -x
-# Downloads the SIPN South 2018 forecast data at the appropriate location
+# Downloads the SIPN South forecast data at the appropriate location
 #
 # F. Massonnet, June 2018
 #               December 2019: update to download 2018-2019 data
-
+#               April 2022 : update for updated data
 
 url=https://nextcloud.cism.ucl.ac.be/s/gTL53xhjp4iQMM8
 
 
-for mystring in 2018-2019 2019-2020
+for mystring in 2018-2019 2019-2020 2020-2021 2021-2022
   do
   for file in `cat list_files_${mystring}.txt`
   do
