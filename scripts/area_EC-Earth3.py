ab7bf89b9cde27b070ef39404e1cb71d64fc1568
François Massonnet <francois.massonnet@uclouvain.be>
2022-07-06 10:57:47 +0200

    Add scripts to use the new EC-Earth3 data

---
+++ scripts/area_EC-Earth3.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+from datetime import date
+from netCDF4 import Dataset
+import numpy as np
+
+subId = "EC-Earth3"
+myyear = "2021-2022"
+nmemb = 10
+
+
+def compute_area(concentration, cellarea, mask = 1):
+  """ Input: - sea ice concentration in %
+               numpy array. if 3-D, time is assumed to be 1st
+             - cellarea: array of grid cell areas (sq. meters)
+             - mask (1 on ocean, 0 on continent)
+
+      Output: Sea ice area in the region defined by the mask
+  """
+  import sys
+  import numpy as np
+
+  if np.max(concentration) < 10.0:
+    sys.exit("(compute_area): concentration seems to not be in percent")
+
+  if len(concentration.shape) == 3:
+    nt, ny, nx = concentration.shape
+    are = np.asarray([np.sum( concentration[jt, :, :] / 100.0 * cellarea * mask) / 1e12 for jt in range(nt)])
+  elif len(concentration.shape) == 2:
+    are = np.sum( concentration / 100.0 * cellarea * mask) / 1e12
+  else:
+    sys.exit("(compute_area): concentration has not 2 nor 3 dimensions")
+
+  return are
+
+for jmemb in np.arange(1, nmemb + 1):
+
+  jjmemb = str(jmemb).zfill(3)
+
+  filein = "../data/" + myyear + "/netcdf/" + subId + "_" + jjmemb + "_concentration.nc"
+
+  # Open file
+  f = Dataset(filein, mode = "r")
+  siconc = f.variables["siconc"][:]
+  lat    = f.variables["latitude"][:]
+  lon    = f.variables["longitude"][:]
+  sftof  = f.variables["sftof"][:]
+  areacello=f.variables["areacello"][:]
+  f.close()
+  ny, nx = sftof.shape
+
+
+  lon[lon < 0.0] = lon[lon < 0.0] + 360.0
+
+  area = compute_area(siconc, areacello, 1.0 * (lat < 0) * (sftof / 100.0))
+
+  with open("../data/" + myyear + "/txt/" + subId + "_" + jjmemb + "_total-area.txt", "w") as file:
+    file.write(",".join(["{0:.4f}".format(a) for a in area]))  # + 1 as python does not take the last bit
+    file.write("\n")
+
+  del area
+
+  with open("../data/" + myyear + "/txt/" + subId + "_" + jjmemb + "_regional-area.txt", "w") as file:
+    # Per longitude bin
+    for j_bin in np.arange(36):
+      print(j_bin)
+      area = compute_area(siconc, areacello, 1.0 * (lat < 0) * (sftof / 100.0) * (lon >= j_bin * 10.0) * (lon < (j_bin + 1) * 10.0))
+      file.write(",".join(["{0:.4f}".format(a) for a in area]))  # + 1 as python does not take the last bit
+      file.write("\n")
---
+++ scripts/interpolate_EC-Earth3.py
@@ -0,0 +1,120 @@
+#!/usr/bin/python
+
+
+# Script to interpolate the monthly sea ice concentration
+# data from monthly to daily
+#
+# For each grid cell, three data points are given for 
+# December, January and February average
+# We are going to look for the quadratic function of time
+# whose time average over December, January and February
+# match the given values
+
+# Our function is f(t) = x_1*t^2 + x_2*t + x_3 (t = 1, ... 90 as DJF has 90 days)
+# Its time-average over interval t1, t2 is:
+# <f> = 1 / nd * x_1 * (t2^3 - t1^3) / 3 + x_2 * (t2^2 - t1^2) / 2 + x_3 * (t2 - t1)
+#     = A(t1, t2, nd) * x_1 + B(t1, t2, nd) * x_2 + C(t1, t2, nd) * x_3
+#     = SIC_12
+# where nd is the number of days of averaging
+#
+# So we need to solve a basic 3x3 system of equations
+# 
+# BX = D with B = [A(t11,t12) B(t11,t12) C(t11,t12) 
+#                  A(t21,t22) B(t21,t22) C(t21,t22)
+#                  A(t31,t32) B(t31,t32) C(t31,t32)]
+# and D = [ SIC_1
+#           SIC_2
+#           SIC_3]
+
+import numpy as np
+from netCDF4 import Dataset
+
+
+thisSeason = "2021-2022" ; yearStart = thisSeason[:4]
+subId = "EC-Earth3"
+nMemb = 10
+
+def A(t1,t2,nd):
+  return 1.0 / 3.0 * (t2 ** 3 - t1 ** 3) / nd
+def B(t1,t2,nd):
+  return 1.0 / 2.0 * (t2 ** 2 - t1 ** 2) / nd
+def C(t1,t2,nd):
+  return 1.0 * (t2 - t1) /nd
+
+t11 = 1.0
+t12 = 31.0
+t21 = 32.0
+t22 = 62.0
+t31 = 63.0
+t32 = 90.0
+
+B = np.array([[A(t11,t12,31), B(t11, t12,31), C(t11,t12,31)], \
+              [A(t21,t22,31), B(t21, t22,31), C(t21,t22,31)], \
+              [A(t31,t32,28), B(t31, t32,28), C(t31,t32,28)], \
+             ])
+
+Binv = np.linalg.inv(B)
+
+
+# For each member
+
+for iMemb in np.arange(1, nMemb + 1):
+    
+    # Read in the data
+    f = Dataset("../data/" + thisSeason + "/netcdf/" + subId + "_" + str(iMemb).zfill(3) + "_concentration_monthly.nc", mode = "r")
+    sic = f.variables["siconc"][:]
+    lat = f.variables["latitude"][:]
+    lon = f.variables["longitude"][:]
+    sft = f.variables["sftof"][:]
+    are = f.variables["areacello"][:]
+    
+    nt, ny, nx = sic.shape
+    f.close()
+    
+    time = np.arange(90)
+    
+    sic_out = np.empty((90, ny, nx))
+    sic_out[:] = np.nan
+
+    
+    for jy in np.arange(ny):
+      for jx in np.arange(nx):
+        D = np.array([[sic[0, jy, jx]], [sic[1, jy, jx]], [sic[2, jy, jx]]])
+        X = np.matmul(Binv, D)
+    
+        tmp = X[0] * time ** 2 + X[1] * time + X[2]
+    
+        tmp[tmp > 100.0] = 100.0
+        tmp[tmp < 0.0  ] = 0.0
+    
+        sic_out[:, jy, jx] = tmp
+    
+    
+    f = Dataset("../data/" + thisSeason + "/netcdf/" + subId + "_" + str(iMemb).zfill(3) + "_concentration.nc", mode = "w")
+    t_d   = f.createDimension("time", None)
+    y_d   = f.createDimension("y", lon.shape[0])
+    x_d   = f.createDimension("x", lon.shape[1])
+    
+    mysic = f.createVariable("siconc", "f4", ("time", "y", "x",))
+    mylon = f.createVariable("longitude", "f4", ("y", "x",))
+    mylat = f.createVariable("latitude", "f4", ("y", "x"))
+    mytime= f.createVariable("time"   , "i4", ("time",))
+    mymask= f.createVariable("sftof", "i4", ("y", "x",))
+    myarea = f.createVariable("areacello", "f4", ("y", "x",))
+    
+    mysic.units = "%"
+    mylon.units = "degrees_east"
+    mylat.units = "degrees_north"
+    mytime.units= "days since " + str(yearStart) + "-12-01"
+    mymask.units= "%"
+    myarea.units = "m2"
+    
+    mylat[:] = lat
+    mylon[:] = lon
+    mytime[:] = time
+    mysic[:] = sic_out
+    mymask[:] = sft
+    myarea[:]  = are
+    
+    f.close()
+
