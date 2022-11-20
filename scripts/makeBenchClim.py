91ea1a87a135e0fac1a8d972e45fd1065474ee81
François Massonnet <francois.massonnet@uclouvain.be>
2022-11-20 10:49:11 +0100

    Update benchmarkclim to produce SIC netcdf in obs

--- scripts/makeBenchClim.py
+++ scripts/makeBenchClim.py
@@ -52,7 +52,7 @@ climDir = "/Users/massonnetf/CLIMDATA/"
 # !!! ATTENTION !!!!
 # The year is referenced to the 1st of December of the summer season, so
 # 2020 --> 2020-2021 = the last season before 2021-2022
-target = "2021-2022"
+target = "2017-2018"
 yeare = int(target[:4]) - 1
 
 # Year to start the collection of climatology. ATTENTION to the convention
@@ -60,8 +60,9 @@ yearb = yeare - 30 + 1
 
 
 
-# Member ID to be incremented each year
+# Member ID, to be incremented each year
 jMemb = 1
+	
 
 for year in np.arange(yearb, yeare + 1):
     print(year)
@@ -75,6 +76,7 @@ for year in np.arange(yearb, yeare + 1):
     
     
     listSia = list()
+    listSic = list()
     # Loop through files
     for d in dates:
         thisYear = d.year
@@ -83,7 +85,8 @@ for year in np.arange(yearb, yeare + 1):
         
         dirData = climDir + "/obs/ice/siconc/OSI-SAF/OSI-450/raw/"
         
-        if thisYear < 2015:
+	# Before 2015 we take OSI-450, after, OSI-401b (unavailabilities restrict the use of one unique product) 
+        if year < 2015:
           dirData = climDir + "/obs/ice/siconc/OSI-SAF/OSI-450/raw/"
           fileName = "ice_conc_sh_ease2-250_cdr-v2p0_" + str(thisYear) + str(thisMonth).zfill(2) + str(thisDay).zfill(2) + "1200.nc"
           cellArea = (25e3) ** 2
@@ -91,21 +94,43 @@ for year in np.arange(yearb, yeare + 1):
           dirData = climDir + "/obs/ice/siconc/OSI-SAF/OSI-401-b/raw/"
           fileName = "ice_conc_sh_polstere-100_multi_" + str(thisYear) + str(thisMonth).zfill(2) + str(thisDay).zfill(2) + "1200.nc"
           cellArea = (10e3) ** 2
+
         if os.path.exists(dirData + fileName):
             
             f = Dataset(dirData + fileName, mode = "r")
-            sic = f.variables["ice_conc"][:]
-
-            listSia.append(compute_area(sic, cellArea)[0])
+            sic = np.squeeze(f.variables["ice_conc"][:])
+            lat = f.variables["lat"][:]
+            lon = f.variables["lon"][:]
+            flag= f.variables["status_flag"][:]
+            ny, nx = lat.shape
+            listSia.append(compute_area(sic, cellArea))
+            listSic.append(sic)
+            
             f.close()
         else:
-           print("File not found: " + dirData + fileName)
-           listSia.append(np.nan)
+            print("File not found: " + dirData + fileName)
+            listSia.append(np.nan)
+            listSic.append(np.full((ny, nx), np.nan))
             
     # Write to txt file
     with open("../data/" + target + "/txt/benchClim_" + str(jMemb).zfill(3) + "_total-area.txt", "w") as file:
         file.write(",".join(["{0:.4f}".format(a) for a in listSia]))  
         file.write("\n")
         
-        
+    # Write to NetCDF file
+    siconcOut = np.array(listSic)
+    siconcOut[siconcOut < 0.0] = np.nan
+    siconcOut[siconcOut > 100.0] = np.nan
+
+    f = Dataset("../data/" + target + "/netcdf/benchClim_" + str(jMemb).zfill(3) + "_concentration.nc", mode = "w")
+    timeDim = f.createDimension("time", None)
+    yDim  =   f.createDimension("y", ny)
+    xDim  = f.createDimension("x", nx)
+    siconcVar = f.createVariable("siconc", np.float64, ("time", "y", "x"))
+    sftofVar  = f.createVariable("sftof", np.float64, ("y", "x"))
+    areacelloVar  = f.createVariable("areacello", np.float64, ("y", "x"))
+    siconcVar[:] = siconcOut
+    sftofVar[:]  = 100 * (flag != 1) # Land-sea mask
+    areacelloVar[:] = np.full((ny, nx), cellArea)
+    f.close()
     jMemb += 1
