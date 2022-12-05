1a73feba8533d112f4993c8ecd6bca8de28747d3
François Massonnet <francois.massonnet@uclouvain.be>
2022-12-05 13:27:00 +0100

    Few usual changes in script parameters

--- scripts/fig6_paper.py
+++ scripts/fig6_paper.py
@@ -102,7 +102,7 @@ exec(open("./namelist.py").read())
 # Script parameters
 # -----------------
 
-seasonId = 0 # Which season to look at
+seasonId = 4 # Which season to look at
 diagId   = 2 # We look at sea ice concentration
 
 # ----
