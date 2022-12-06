ecd3c79a8aff7dab1c70b5a71a6329c47c3e8a7e
François Massonnet <francois.massonnet@uclouvain.be>
2022-12-06 13:56:31 +0100

    Initiate 2022-2023 and add MetOffice

---
+++ scripts/namelist_2022-2023.py
@@ -0,0 +1,25 @@
+#          Group               Forecast ID      Color for plots [string]     Comment
+#          name [string]       list [list]
+np.random.seed(10)
+info = [ 
+#    [ "barreira",         range(1, 3  + 1),               [0.65, 0.81, 0.89], "$^{s}$"  ],  \
+#    [ "benchClim",        range(1, 30  + 1),               [0.2, 0.2, 0.2]   , "$^{b}$"  ],
+#    [ "CanSIPSv2",        range(1, 20 + 1), "#1898e0", "$^d$"    ],  \
+#    [ "cmcc",             range(1, 50 + 1), "#00b2ed", "$^d$"    ],  \
+#    [ "CNRM",             range(1, 51 + 1), "#00bb62", "$^d$"    ],  \
+#    [ "BSC",              range(1, 10 + 1), "red"    , "$^d$"    ],  \
+#    [ "ecmwf",            range(1, 51 + 1)  , [0.69, 0.35, 0.16], "$^d$"    ],  \
+#    [ "FIO-ESM",          [1],              "#8bcd45", "$^d$"    ],  \
+#    [ "gfdl",             range(1, 30 + 1), "#dbe622", "$^d$"    ],  \
+#    [ "Lamont",           [1],              "#f9c410", "$^{s,i}$"],  \
+#    [ "Meier-NSIDC",      [1],              "#f89e13", "$^s$"    ],
+    [ "MetOffice",        range(1, 42 + 1), "#ff8040", "$^d$"    ],  \
+#    [ "NASA-GSFC",        [1],              "#fb4c27", "$^{s}$"  ],  \
+#    [ "NicoSun",          range(1, 3 + 1),  "#fb4865", "$^s$"    ],  \
+#    [ "SINTEX-F2",        range(1, 24 + 1), "#d24493", "$^d$"    ],  \
+#    [ "SYSU"    ,         [1],              "#8f57bf", "$^s$"    ],  \
+#    [ "ucl",              range(1, 10 + 1), "#645ccc", "$^d$"    ],  \
+
+       ]
+
+
---
+++ scripts/namelist_spatial_2022-2023.py
@@ -0,0 +1,14 @@
+#          Group     Nb forecasts  Color for plots     Comment
+#          name
+np.random.seed(10)
+info = [ 
+          ["MetOffice",  range(1, 42 + 1), "#0077D4",   "$^d$"    ],  \
+#          ["ucl",        range(1, 10 + 1), "#005BC3",   "$^d$"    ],  \
+#          ["CNRM",       range(1, 51 + 1), "#00A2E1",   "$^d$"    ],  \
+#          ["NicoSun",    range(1, 3 + 1),  "#CA4E00",   "$^s$"    ],  \
+#          ["barreira",   [1]             , "#E76F00",   "$^s$"    ],  \
+#          ["Lamont",     [1],              "#FF8642",   "$^{s,i}$"],  \
+#          ["cmcc"  ,     range(1, 50 + 1), "#0070B2",   "$^d$"    ],  \
+#          [ "gfdl"   ,   range(1, 30 + 1), "#0047B6",   "$^d$"    ],  \
+#          ["SYSU"      , [1]             , "#FF963B",   "$^s$"    ],  \
+       ]
