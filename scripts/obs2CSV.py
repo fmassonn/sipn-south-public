8d3858b5fa27cce9784b2daa8d8598b3edb8ff69
François Massonnet <francois.massonnet@uclouvain.be>
2022-12-28 11:45:38 +0100

    Add multiplication by mask in total area; add calculation of area south of 60S

--- scripts/obs2CSV.py
+++ scripts/obs2CSV.py
@@ -116,8 +116,11 @@ for year in np.arange(yearb, yeare + 1):
             
             # Compute sea ice area for that period
             # ------------------------------------
-            areatot = compute_area(siconc[t1:t2 + 1, :, :], cellarea, mask = 1.0 * (lat < 0.0)) # + 1 because of Python indexing convention
+            areatot = compute_area(siconc[t1:t2 + 1, :, :], cellarea, mask = 1.0 * (sftof == 100.0) * (lat < 0.0)) # + 1 because of Python indexing convention
             print(areatot)
+
+            # Print area south of 50°S
+            print("Oceanic area south of 60°S: " + str(np.sum(cellarea * (sftof == 100.0))))
             # Save as CSV file
             # ----------------
     
