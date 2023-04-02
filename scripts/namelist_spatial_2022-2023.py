ef411424136750349a526b5e9ad7a71d9e0ce2c8
François Massonnet <francois.massonnet@uclouvain.be>
2023-04-02 19:51:10 +0200

    Namelist spatial

--- scripts/namelist_spatial_2022-2023.py
+++ scripts/namelist_spatial_2022-2023.py
@@ -2,16 +2,16 @@
 #          name
 np.random.seed(10)
 info = [ 
-#          ["MetOffice",  range(1, 42 + 1), "#0077D4",   "$^d$"    ],  \
+          ["MetOffice",  range(1, 42 + 1), "#0077D4",   "$^d$"    ],  \
           ["BSC"      ,  range(1, 10 + 1), "#XYZT",     "$^d$"    ],
-#          ["ucl",        range(1, 10 + 1), "#005BC3",   "$^d$"    ],  \
-#          ["NicoSun",    range(1, 3 + 1),  "#CA4E00",   "$^s$"    ],  \
+          ["ucl",        range(1, 10 + 1), "#005BC3",   "$^d$"    ],  \
+          ["NicoSun",    range(1, 3 + 1),  "#CA4E00",   "$^s$"    ],  \
           ["emc",        [1],              "#ABCDE",    "$^d$"    ], \
-          ["barreira",   range(1, 4 + 1), "#E76F00",   "$^s$"    ],  \
-#          ["Lamont",     [1],              "#FF8642",   "$^{s,i}$"],  \
-#          ["cmcc"  ,     range(1, 50 + 1), "#0070B2",   "$^d$"    ],  \
+          ["barreira",   range(1, 2 + 1), "#E76F00",   "$^s$"    ],  \
+          ["Lamont",     [1],              "#FF8642",   "$^{s,i}$"],  \
+          ["cmcc"  ,     range(1, 50 + 1), "#0070B2",   "$^d$"    ],  \
           [ "gfdl"   ,   range(1, 30 + 1), "#0047B6",   "$^d$"    ],  \
-           ["SYSU_SML_ConvLSTM", [1],              "#12AD2B", "$^s$"    ],
-           ["SYSU-SML-MLM",      [1],              "#5E53C7", "$^s$"    ],
-	   ["SYSU-SML-KNN",      [1],              "#005BC3", "$^s$"    ],
+          ["SYSU-SML-ConvLSTM", [1],              "#12AD2B", "$^s$"    ],
+          ["SYSU-SML-MLM",      [1],              "#5E53C7", "$^s$"    ],
+          ["SYSU-SML-KNN",      [1],              "#005BC3", "$^s$"    ],
        ]
