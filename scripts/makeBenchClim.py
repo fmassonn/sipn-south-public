33c15a21e82637c6537339db41ddf2afd54d2fa2
François Massonnet <francois.massonnet@uclouvain.be>
2024-03-20 16:16:50 +0100

    fix climatology

--- scripts/makeBenchClim.py
+++ scripts/makeBenchClim.py
@@ -6,7 +6,9 @@ Created on Wed Jun  1 15:15:47 2022
 @author: massonnetf
 """
 
-# Creates benchmark climatology file by downloading the daily 
+# Creates benchmark climatology forecasts by retrieving the 
+# observations of previous years
+
 
 import numpy as np
 from datetime import datetime
@@ -41,30 +43,36 @@ def compute_area(concentration, cellarea, mask = 1):
   return are
 
 
-
+# Source dir
 climDir = "/Users/massonnetf/CLIMDATA/"
 
-
-# Current forecasting season. The season before will be the last one to create a climatologyL
+# Year of the 1st of December starting the summer season
+# Last year to be processed. That's the year of the forecast minus one
+#
+# !!! ATTENTION !!!!
+# The year is referenced to the 1st of December of the summer season, so
+# 2020 --> 2020-2021 = the last season before 2021-2022
 target = "2023-2024"
-# The forecasting period (to be added in the file name)
-forecastPeriod = target[:4] + "1201-" + target[5:] + "0228"
-
 yeare = int(target[:4]) - 1
 
 # Year to start the collection of climatology. ATTENTION to the convention
 yearb = yeare - 30 + 1 
+forecastPeriod = target[:4] + "1201-" + target[5:9] + "0228"
 
 
-
-# Member ID to be incremented each year
+# Member ID, to be incremented each year
 jMemb = 1
+	
 
 for year in np.arange(yearb, yeare + 1):
     print(year)
     
     #  Create date axis
-    d0 = datetime(year, 12, 1    )
+    if target == "2017-2018":
+      d0 = datetime(year + 1, 2, 1)
+    else:
+      d0 = datetime(year, 12, 1    )
+
     d1 = datetime(year + 1, 2, 28)
     nd = (d1 - d0).days + 1
     
@@ -72,6 +80,7 @@ for year in np.arange(yearb, yeare + 1):
     
     
     listSia = list()
+    listSic = list()
     # Loop through files
     for d in dates:
         thisYear = d.year
@@ -80,7 +89,8 @@ for year in np.arange(yearb, yeare + 1):
         
         dirData = climDir + "/obs/ice/siconc/OSI-SAF/OSI-450/raw/"
         
-        if thisYear < 2015:
+	# Before 2015 we take OSI-450, after, OSI-401b (unavailabilities restrict the use of one unique product) 
+        if year < 2015:
           dirData = climDir + "/obs/ice/siconc/OSI-SAF/OSI-450/raw/"
           fileName = "ice_conc_sh_ease2-250_cdr-v2p0_" + str(thisYear) + str(thisMonth).zfill(2) + str(thisDay).zfill(2) + "1200.nc"
           cellArea = (25e3) ** 2
@@ -88,23 +98,48 @@ for year in np.arange(yearb, yeare + 1):
           dirData = climDir + "/obs/ice/siconc/OSI-SAF/OSI-401-b/raw/"
           fileName = "ice_conc_sh_polstere-100_multi_" + str(thisYear) + str(thisMonth).zfill(2) + str(thisDay).zfill(2) + "1200.nc"
           cellArea = (10e3) ** 2
+
         if os.path.exists(dirData + fileName):
             
             f = Dataset(dirData + fileName, mode = "r")
-            sic = f.variables["ice_conc"][:]
-
-            listSia.append(compute_area(sic, cellArea)[0])
-            f.close()
+            sic = np.squeeze(f.variables["ice_conc"][:])
+            lat = f.variables["lat"][:]
+            lon = f.variables["lon"][:]
+            flag= f.variables["status_flag"][:]
+            ny, nx = lat.shape
+            listSia.append(compute_area(sic, cellArea))
+            listSic.append(sic)
             
-         
+            f.close()
         else:
-           print("File not found: " + dirData + fileName)
-           listSia.append(np.nan)
+            print("File not found: " + dirData + fileName)
+            listSia.append(np.nan)
+            listSic.append(np.full((ny, nx), np.nan))
             
     # Write to txt file
     with open("../data/" + target + "/txt/climatology_" + str(jMemb).zfill(3) + "_" + forecastPeriod + "_total-area.txt", "w") as file:
         file.write(",".join(["{0:.4f}".format(a) for a in listSia]))  
         file.write("\n")
         
-        
+    # Write to NetCDF file
+    siconcOut = np.array(listSic)
+    siconcOut[siconcOut < 0.0] = np.nan
+    siconcOut[siconcOut > 100.0] = np.nan
+
+    f = Dataset("../data/" + target + "/netcdf/climatology_" + str(jMemb).zfill(3) + "_" + forecastPeriod + "_concentration.nc", mode = "w")
+    timeDim = f.createDimension("time", None)
+    yDim  =   f.createDimension("y", ny)
+    xDim  = f.createDimension("x", nx)
+    siconcVar = f.createVariable("siconc", np.float64, ("time", "y", "x"))
+    sftofVar  = f.createVariable("sftof", np.float64, ("y", "x"))
+    areacelloVar  = f.createVariable("areacello", np.float64, ("y", "x"))
+    latVar        = f.createVariable("latitude", np.float64, ("y", "x"))
+    lonVar        = f.createVariable("longitude", np.float64, ("y", "x"))
+   
+    siconcVar[:] = siconcOut
+    sftofVar[:]  = 100 * (flag != 1) # Land-sea mask
+    latVar[:] = lat
+    lonVar[:] = lon
+    areacelloVar[:] = np.full((ny, nx), cellArea)
+    f.close()
     jMemb += 1
