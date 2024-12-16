0156eb6a287667141c0367183457ae6106df1d82
François Massonnet <francois.massonnet@uclouvain.be>
2024-12-16 13:07:32 +0100

    update spatial 24-25

--- scripts/spatial_sip.py
+++ scripts/spatial_sip.py
@@ -20,7 +20,7 @@ from   datetime import datetime
 
 # Script parameters
 
-myyear = "2023-2024"  # label with the year investigated (2017-2018, 2018-2019, ...)
+myyear = "2024-2025"  # label with the year investigated (2017-2018, 2018-2019, ...)
 plotobs = True       # Add obs as reference or not (False if forecast mode)
 
 # Load namelist
